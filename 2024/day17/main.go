b3ea62acac5c1d1f27228f7e4ac1d525b33c5218
Aaron Joyce <2ajoyce@gmail.com>
2024-12-25 18:00:18 -0600

    day17 - Part2 - BFS that works on example

--- 2024/day17/main.go
+++ 2024/day17/main.go
@@ -9,8 +9,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-
-	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
@@ -54,7 +52,7 @@ func main() {
 		fmt.Println("Error parsing input:", err)
 		return
 	}
-	results, err := solve(input, PARALLELISM)
+	results, err := solve(input)
 	if err != nil {
 		fmt.Println("Error solving 1:", err)
 		return
@@ -142,111 +140,174 @@ func parseLines(lines []string) (*day17.Computer, error) {
 	return computer, nil
 }
 
-func solve(comp *day17.Computer, WORKER_COUNT int) ([]string, error) {
-	fmt.Printf("Beginning solve with %d workers\n", WORKER_COUNT)
-
-	// Calculate the expected output based on the initial state of the computer
-	expectedOutput := ""
-	opcodes := comp.GetOpcodes()
-	for _, opcode := range opcodes {
-		expectedOutput = expectedOutput + fmt.Sprintf("%d,", opcode)
+func solve(comp *day17.Computer) ([]string, error) {
+	fmt.Println("Beginning single-threaded solve")
+	results := findRangesBFS(comp.GetOpcodes())
+	for i, r := range results {
+		fmt.Printf("Result %d: %s\n", i, r.String())
 	}
-	expectedOutput = strings.TrimSuffix(expectedOutput, ",")
 
-	testRegAValue := big.NewInt(216134799294990)
-	var optimalRegAValue *big.Int = nil
-	bar := progressbar.Default(testRegAValue.Int64(), "Solving")
+	return nil, nil
+}
 
-	// Create a channel to distribute tasks to workers
-	tasks := make(chan *big.Int, WORKER_COUNT)
-	type inputOutput struct {
-		input  string
-		output string
+type Range struct {
+	Match        string
+	Start        *big.Int
+	End          *big.Int
+	Index        int
+	OutputLength int
+}
+
+func (r Range) String() string {
+	return fmt.Sprintf("{Start: %s, End: %s, Index: %d, Match: %s, OutputLength: %d}", r.Start.String(), r.End.String(), r.Index, r.Match, r.OutputLength)
+}
+
+type Increment struct {
+	Input  *big.Int
+	Output string
+}
+
+func findRanges(c *day17.Computer, r Range) []Range {
+	DEBUG := os.Getenv("DEBUG") == "true"
+	eight := big.NewInt(8)
+	increment := big.NewInt(0).Exp(eight, big.NewInt(int64(r.Index)), nil)
+	if DEBUG {
+		fmt.Printf("Start: %s, End: %s, Increment: %s(8^%d), Match: %s\n", r.Start.String(), r.End.String(), increment.String(), r.Index, r.Match)
 	}
-	results := make(chan inputOutput, WORKER_COUNT)
 
-	// Read results from workers
-	go func() {
-		header := "input,output"
-		err := aocUtils.AppendToFile("data.csv", []string{header})
+	increments := []Increment{}
+	// Collect the increments
+	for i := r.End; i.Cmp(r.Start) >= 0; i = big.NewInt(0).Sub(i, increment) {
+		cloneComp := c.Clone()
+		cloneComp.SetRegisterA(i)
+		output, err := SolveComputer(0, cloneComp)
 		if err != nil {
-			panic(fmt.Errorf("unexpected error: %v", err))
+			fmt.Printf("Error solving with value %s: %v\n", i.String(), err)
+			continue
 		}
-		comparisonString := strings.ReplaceAll(expectedOutput, ",", "")
-		aocUtils.AppendToFile("data.csv", []string{comparisonString})
-		for result := range results {
-			if len(result.output) < len(comparisonString) {
-				break
-			}
-			if !strings.HasSuffix(result.output, "155530") {
-				continue
-			}
-			data := fmt.Sprintf("%s,%s", result.input, result.output)
-			err := aocUtils.AppendToFile("data.csv", []string{data})
-			if err != nil {
-				panic(fmt.Errorf("unexpected error: %v", err))
-			}
+		outputWithoutCommas := strings.ReplaceAll(output, ",", "")
+		if len(outputWithoutCommas) < r.OutputLength {
+			break
 		}
-	}()
+		increments = append(increments, Increment{Input: i, Output: outputWithoutCommas})
 
-	var wg sync.WaitGroup
-	// Launch worker goroutines
-	for i := 0; i < WORKER_COUNT; i++ {
-		wg.Add(1)
-		go func(workerId int) {
-			defer wg.Done()
-			for regA := range tasks {
-				cloneComp := comp.Clone()
-				cloneComp.SetRegisterA(regA)
-
-				workerOutput, err := SolveComputer(workerId, cloneComp)
-				if err != nil {
-					panic(fmt.Errorf("unexpected error: %v", err))
-				}
-
-				if optimalRegAValue != nil {
-					break
-				}
-				if workerOutput == expectedOutput {
-					fmt.Printf("\nWorker %d found optimal RegA value: %s\n", workerId, regA)
-					optimalRegAValue = big.NewInt(0).Set(regA)
-				}
-				// Send the result to the results channel
-				regAString := regA.String()
-				workerOutput = strings.ReplaceAll(workerOutput, ",", "")
-				results <- inputOutput{input: regAString, output: workerOutput}
+		if DEBUG {
+			octalI := fmt.Sprintf("%s", i.Text(8))
+			outputString := fmt.Sprintf("Value: %-20s Octal: %-20s Output: %-20s", i.String(), octalI, output)
+			// If i is divisible by 8
+			if big.NewInt(0).Mod(i, big.NewInt(8)).Cmp(big.NewInt(0)) == 0 {
+				outputString = fmt.Sprintf("\033[32m%s\033[0m", outputString)
 			}
-		}(i)
+			fmt.Println(outputString)
+		}
 	}
 
-	// Distribute tasks to workers
-	tasksDistributed := 0
-	go func() {
-		fmt.Printf("Ignoring testRegAValue(%s) for now", testRegAValue)
-		i := 1
-		for optimalRegAValue == nil {
-			increment := big.NewInt(0).Exp(big.NewInt(8), big.NewInt(int64(6)), nil)
-			testRegAValue = big.NewInt(0).Sub(testRegAValue, increment)
-			tasks <- testRegAValue
-			bar.Add(1)
-			tasksDistributed++
-			i = i * 10
+	// Check the increments for matches
+	ranges := []Range{}
+	var rangeStart *big.Int = nil
+	var rangeEnd *big.Int = nil
+	for i, inc := range increments {
+		compareString := inc.Output[r.Index-1:]
+		if compareString == r.Match && rangeEnd == nil {
+			fmt.Printf("End found at %d\n", i)
+			if len(r.Match) == len(inc.Output) {
+				rangeEnd = big.NewInt(0).Set(increments[i].Input)
+				rangeStart = big.NewInt(0).Set(increments[i].Input)
+			} else if i == 0 {
+				rangeEnd = big.NewInt(0).Set(r.End)
+			} else {
+				rangeEnd = big.NewInt(0).Set(increments[i-1].Input)
+			}
 		}
-		close(tasks)
-	}()
+		if compareString != r.Match && rangeEnd != nil {
+			fmt.Printf("Start found: %s\n", inc)
+			rangeStart = big.NewInt(0).Set(inc.Input)
+		}
+		if rangeStart != nil && rangeEnd != nil {
+			ranges = append(ranges, Range{Start: rangeStart, End: rangeEnd, Index: r.Index - 1, Match: r.Match, OutputLength: r.OutputLength})
+			rangeStart = nil
+			rangeEnd = nil
+		}
+	}
+	if rangeEnd != nil && rangeStart == nil {
+		fmt.Printf("No start found, setting start to start of range: %s\n", r.Start)
+		rangeStart = r.Start
+		ranges = append(ranges, Range{Start: rangeStart, End: rangeEnd, Index: r.Index - 1, Match: r.Match, OutputLength: r.OutputLength})
+	}
 
-	wg.Wait()
-	output := fmt.Sprintf("Lowest RegA Value: %s", optimalRegAValue)
-	fmt.Println(output)
-	return []string{output}, nil
+	return ranges
+}
+
+func findRangesBFS(opCodes []day17.Opcode) []*big.Int {
+	// Initialize the result array for complete matches
+	var completeMatches []*big.Int
+
+	fmt.Println("Initializing computer with opcodes")
+	comp := day17.NewComputer()
+	comp.SetOpcodes(opCodes)
+
+	index := len(opCodes)
+	fmt.Printf("Opcode length: %d\n", index)
+
+	initialEnd := big.NewInt(0).Exp(big.NewInt(8), big.NewInt(int64(index)), nil)
+	initialEnd = big.NewInt(0).Sub(initialEnd, big.NewInt(8))
+	fmt.Printf("Initial range: 0 to %s\n", initialEnd.String())
+
+	// Initialize the heap (queue) with the initial range
+	initialRange := Range{
+		Start:        big.NewInt(0),
+		End:          initialEnd,
+		Index:        index,
+		Match:        fmt.Sprintf("%d", opCodes[index-1]),
+		OutputLength: len(opCodes),
+	}
+	queue := []Range{initialRange}
+	fmt.Println("Initial range added to the queue")
+
+	// Perform BFS
+	for len(queue) > 0 {
+		fmt.Printf("Queue size: %d\n", len(queue))
+		// Dequeue the next range to process
+		currentRange := queue[0]
+		queue = queue[1:]
+
+		currentRange.Match = ""
+		for _, opcode := range opCodes[currentRange.Index-1:] {
+			currentRange.Match += fmt.Sprintf("%d", opcode)
+		}
+
+		fmt.Printf("Processing range: %s\n", currentRange)
+
+		// Call findRanges to get new ranges from the current range
+		newRanges := findRanges(comp, currentRange)
+		fmt.Printf("Found %d new ranges\n", len(newRanges))
+
+		// Process each range found
+		for _, r := range newRanges {
+			fmt.Printf("Processing new range: %s\n", r)
+			// Check if this range is a complete match
+			if r.Start.Cmp(r.End) == 0 {
+				fmt.Printf("Found complete match: %s\n", r.Start.String())
+				// Add to complete matches
+				completeMatches = append(completeMatches, r.Start)
+			} else {
+				// Enqueue for further processing
+				fmt.Println("Enqueuing new range for processing")
+				queue = append(queue, r)
+			}
+		}
+	}
+
+	fmt.Printf("Total complete matches found: %d\n", len(completeMatches))
+	return completeMatches
 }
 
 func SolveComputer(workerId int, comp *day17.Computer) (string, error) {
-	DEBUG := os.Getenv("DEBUG") == "true"
-	if DEBUG {
-		fmt.Printf("Worker %d: Beginning solve\n", workerId)
-		fmt.Printf("Worker %d: Initial State: %s\n", workerId, comp)
-	}
+	// DEBUG := os.Getenv("DEBUG") == "true"
+	// if DEBUG {
+	// 	fmt.Printf("Worker %d: Beginning solve\n", workerId)
+	// 	fmt.Printf("Worker %d: Initial State: %s\n", workerId, comp)
+	// }
 	var loopDetection = 0
 
 	// Get the opcodes from the computer
@@ -272,13 +333,13 @@ func SolveComputer(workerId int, comp *day17.Computer) (string, error) {
 		// Get the opcode at the instruction pointer
 		opcode := opcodes[ip]
 
-		if DEBUG {
-			fmt.Printf("Worker %d: Executing", workerId)
-			fmt.Printf("Worker %d:     Computer State: %s\n", workerId, comp)
-			fmt.Printf("Worker %d:     Instruction pointer %d\n", workerId, ip)
-			fmt.Printf("Worker %d:     Opcode %d\n", workerId, opcode)
-			fmt.Printf("Worker %d:     Operand: %d\n", workerId, opcodes[ip+1])
-		}
+		// if DEBUG {
+		// 	fmt.Printf("Worker %d: Executing", workerId)
+		// 	fmt.Printf("Worker %d:     Computer State: %s\n", workerId, comp)
+		// 	fmt.Printf("Worker %d:     Instruction pointer %d\n", workerId, ip)
+		// 	fmt.Printf("Worker %d:     Opcode %d\n", workerId, opcode)
+		// 	fmt.Printf("Worker %d:     Operand: %d\n", workerId, opcodes[ip+1])
+		// }
 
 		// Get the function associated with the opcode
 		fn, err := opcode.GetInstruction()
@@ -286,9 +347,9 @@ func SolveComputer(workerId int, comp *day17.Computer) (string, error) {
 			return "", fmt.Errorf("error in Worker %d: getting instruction for opcode %d at instruction pointer %d: %v", workerId, opcode, ip, err)
 		}
 
-		if DEBUG {
-			fmt.Printf("Worker %d:     Executing Function\n", workerId)
-		}
+		// if DEBUG {
+		// 	fmt.Printf("Worker %d:     Executing Function\n", workerId)
+		// }
 
 		// Execute the function
 		err = fn(comp, opcodes[ip+1])
@@ -296,15 +357,15 @@ func SolveComputer(workerId int, comp *day17.Computer) (string, error) {
 			return "", fmt.Errorf("error in Worker %d: executing opcode %d: %v", workerId, opcode, err)
 		}
 
-		if DEBUG {
-			fmt.Printf("Worker %d:     Fetching New Instruction Pointer\n", workerId)
-		}
+		// if DEBUG {
+		// 	fmt.Printf("Worker %d:     Fetching New Instruction Pointer\n", workerId)
+		// }
 
 		// Get the instruction pointer from the computer
 		newIp := comp.GetInstructionPointer()
-		if DEBUG {
-			fmt.Printf("Worker %d:     New Instruction Pointer: %d\n", workerId, newIp)
-		}
+		// if DEBUG {
+		// 	fmt.Printf("Worker %d:     New Instruction Pointer: %d\n", workerId, newIp)
+		// }
 		if newIp == ip {
 			loopDetection++
 			if loopDetection > 10 {
@@ -314,14 +375,15 @@ func SolveComputer(workerId int, comp *day17.Computer) (string, error) {
 			loopDetection = 0
 		}
 		ip = newIp
+		if ip < 0 || ip >= len(opcodes) {
+			close(comp.Output)
+		}
 	}
-
-	close(comp.Output)
 	workerWg.Wait()
-	if DEBUG {
-		fmt.Printf("Worker %d: Solve complete", workerId)
-		fmt.Printf("Worker %d: Final State: %s\n", workerId, comp)
-		fmt.Printf("Worker %d: Output: %s\n", workerId, workerOutput)
-	}
+	// if DEBUG {
+	// 	fmt.Printf("Worker %d: Solve complete", workerId)
+	// 	fmt.Printf("Worker %d: Final State: %s\n", workerId, comp)
+	// 	fmt.Printf("Worker %d: Output: %s\n", workerId, workerOutput)
+	// }
 	return workerOutput, nil
 }
--- 2024/day17/main_test.go
+++ 2024/day17/main_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math/big"
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -65,25 +64,6 @@ func TestSolveComputer1(t *testing.T) {
 	}
 }
 
-func TestSolveComputerFinal(t *testing.T) {
-	os.Setenv("DEBUG", "false")
-	comp := day17.NewComputer()
-	comp.SetRegisterA(big.NewInt(60645977707275))
-	// 03555114305731420
-	// 2413750341155530
-	comp.SetOpcodes([]day17.Opcode{2, 4, 1, 3, 7, 5, 0, 3, 4, 1, 1, 5, 5, 5, 3, 0})
-	expectedOutput := "2,4,1,3,7,5,0,3,4,1,1,5,5,5,3,0"
-
-	output, err := SolveComputer(0, comp)
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-
-	if output != expectedOutput {
-		t.Errorf("Expected output to be \n'%s' but got: \n'%s'", expectedOutput, output)
-	}
-}
-
 func TestSolveComputer2(t *testing.T) {
 	comp := day17.NewComputer()
 	comp.SetRegisterA(big.NewInt(10))
@@ -133,40 +113,263 @@ func TestSolveCanSolvePart2(t *testing.T) {
 		t.Errorf("Expected output to be '%s', but got: %s", expectedOutput, output)
 	}
 }
-
-func TestMainPart2Example(t *testing.T) {
-	os.Setenv("DEBUG", "false")
-	inputData := []string{
-		"Register A: 2024\n",
-		"Register B: 0\n",
-		"Register C: 0\n",
-		"\n",
-		"Program: 0,3,5,4,3,0\n",
+func compareRanges(t *testing.T, r, expected Range, i int) bool {
+	if r.Start.Cmp(expected.Start) != 0 {
+		t.Errorf("Expected range %d to start at %v, but got %v", i, expected.Start, r.Start)
+		return false
 	}
-	os.WriteFile(INPUT_FILE, []byte(strings.Join(inputData, "")), 0644)
-	total := 117440
-
-	main()
-
-	expectedContent := fmt.Sprintf("Lowest RegA Value: %d", total)
-	validateOutput(t, expectedContent)
+	if r.End.Cmp(expected.End) != 0 {
+		t.Errorf("Expected range %d to end at %v, but got %v", i, expected.End, r.End)
+		return false
+	}
+	if r.Index != expected.Index {
+		t.Errorf("Expected range %d to have index %d, but got %d", i, expected.Index, r.Index)
+		return false
+	}
+	if r.Match != expected.Match {
+		t.Errorf("Expected range %d to have match %s, but got %s", i, expected.Match, r.Match)
+		return false
+	}
+	if r.OutputLength != expected.OutputLength {
+		t.Errorf("Expected range %d to have match %d, but got %d", i, expected.OutputLength, r.OutputLength)
+		return false
+	}
+	return true
 }
 
-func TestMainPart2ExampleParallel(t *testing.T) {
-	os.Setenv("DEBUG", "false")
-	os.Setenv("Parallelism", "5")
-	inputData := []string{
-		"Register A: 2024\n",
-		"Register B: 0\n",
-		"Register C: 0\n",
-		"\n",
-		"Program: 0,3,5,4,3,0\n",
+func TestFindRangesIndexOne(t *testing.T) {
+	comp := day17.NewComputer()
+	comp.SetRegisterA(big.NewInt(117440))
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+	comp.SetOpcodes(opCodes)
+
+	index := len(opCodes)
+	match := fmt.Sprintf("%d", opCodes[index-1])
+	searchSpace := Range{Start: big.NewInt(0), End: big.NewInt(262144 - 8), Index: index, Match: match, OutputLength: len(opCodes)}
+	ranges := findRanges(comp, searchSpace)
+
+	expectedRanges := []Range{
+		{Start: searchSpace.Start, End: searchSpace.End, Index: searchSpace.Index - 1, Match: searchSpace.Match, OutputLength: len(opCodes)},
 	}
-	os.WriteFile(INPUT_FILE, []byte(strings.Join(inputData, "")), 0644)
-	total := 117440
 
-	main()
+	if len(ranges) != len(expectedRanges) {
+		t.Errorf("Expected %d ranges, but got %d", len(expectedRanges), len(ranges))
+	}
 
-	expectedContent := fmt.Sprintf("Lowest RegA Value: %d", total)
-	validateOutput(t, expectedContent)
+	for i, r := range ranges {
+		compareRanges(t, r, expectedRanges[i], i)
+	}
+}
+
+func TestFindRangesIndexTwo(t *testing.T) {
+	comp := day17.NewComputer()
+	comp.SetRegisterA(big.NewInt(117440))
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+	comp.SetOpcodes(opCodes)
+
+	index := len(opCodes) - 1
+	// Convert the opcodes from the index to the end of the slice to a string
+	match := ""
+	for _, opcode := range opCodes[index-1:] {
+		match += fmt.Sprintf("%d", opcode)
+	}
+	searchSpace := Range{Start: big.NewInt(0), End: big.NewInt(262136), Index: index, Match: match, OutputLength: len(opCodes)}
+	ranges := findRanges(comp, searchSpace)
+
+	expectedRanges := []Range{
+		{Start: big.NewInt(98296), End: big.NewInt(163832), Index: searchSpace.Index - 1, Match: searchSpace.Match, OutputLength: len(opCodes)},
+	}
+
+	if len(ranges) != len(expectedRanges) {
+		t.Errorf("Expected %d ranges, but got %d", len(expectedRanges), len(ranges))
+	}
+
+	for i, r := range ranges {
+		compareRanges(t, r, expectedRanges[i], i)
+	}
+}
+
+func TestFindRangesIndexThree(t *testing.T) {
+	comp := day17.NewComputer()
+	comp.SetRegisterA(big.NewInt(117440))
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+	comp.SetOpcodes(opCodes)
+
+	index := len(opCodes) - 2
+	// Convert the opcodes from the index to the end of the slice to a string
+	match := ""
+	for _, opcode := range opCodes[index-1:] {
+		match += fmt.Sprintf("%d", opcode)
+	}
+	searchSpace := Range{Start: big.NewInt(98296), End: big.NewInt(163832), Index: index, Match: match, OutputLength: len(opCodes)}
+	ranges := findRanges(comp, searchSpace)
+
+	expectedRanges := []Range{
+		{Start: big.NewInt(114680), End: big.NewInt(122872), Index: searchSpace.Index - 1, Match: searchSpace.Match, OutputLength: len(opCodes)},
+	}
+
+	if len(ranges) != len(expectedRanges) {
+		t.Errorf("Expected %d ranges, but got %d", len(expectedRanges), len(ranges))
+	}
+
+	for i, r := range ranges {
+		compareRanges(t, r, expectedRanges[i], i)
+	}
+}
+
+func TestFindRangesIndexFour(t *testing.T) {
+	comp := day17.NewComputer()
+	comp.SetRegisterA(big.NewInt(117440))
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+	comp.SetOpcodes(opCodes)
+
+	index := len(opCodes) - 3
+	// Convert the opcodes from the index to the end of the slice to a string
+	match := ""
+	for _, opcode := range opCodes[index-1:] {
+		match += fmt.Sprintf("%d", opcode)
+	}
+	searchSpace := Range{Start: big.NewInt(114680), End: big.NewInt(122872), Index: index, Match: match, OutputLength: len(opCodes)}
+	ranges := findRanges(comp, searchSpace)
+
+	expectedRanges := []Range{
+		{Start: big.NewInt(117240), End: big.NewInt(118264), Index: searchSpace.Index - 1, Match: searchSpace.Match, OutputLength: len(opCodes)},
+	}
+
+	if len(ranges) != len(expectedRanges) {
+		t.Errorf("Expected %d ranges, but got %d", len(expectedRanges), len(ranges))
+	}
+
+	for i, r := range ranges {
+		compareRanges(t, r, expectedRanges[i], i)
+	}
+}
+
+func TestFindRangesIndexFive(t *testing.T) {
+	comp := day17.NewComputer()
+	comp.SetRegisterA(big.NewInt(117440))
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+	comp.SetOpcodes(opCodes)
+
+	index := len(opCodes) - 4
+	// Convert the opcodes from the index to the end of the slice to a string
+	match := ""
+	for _, opcode := range opCodes[index-1:] {
+		match += fmt.Sprintf("%d", opcode)
+	}
+	searchSpace := Range{Start: big.NewInt(117240), End: big.NewInt(118264), Index: index, Match: match, OutputLength: len(opCodes)}
+	ranges := findRanges(comp, searchSpace)
+
+	expectedRanges := []Range{
+		{Start: big.NewInt(117432), End: big.NewInt(117560), Index: searchSpace.Index - 1, Match: searchSpace.Match, OutputLength: len(opCodes)},
+	}
+
+	if len(ranges) != len(expectedRanges) {
+		t.Errorf("Expected %d ranges, but got %d", len(expectedRanges), len(ranges))
+	}
+
+	for i, r := range ranges {
+		compareRanges(t, r, expectedRanges[i], i)
+	}
+}
+
+func TestFindRangesIndexSix(t *testing.T) {
+	comp := day17.NewComputer()
+	comp.SetRegisterA(big.NewInt(117440))
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+	comp.SetOpcodes(opCodes)
+
+	index := len(opCodes) - 5
+	// Convert the opcodes from the index to the end of the slice to a string
+	match := ""
+	for _, opcode := range opCodes[index-1:] {
+		match += fmt.Sprintf("%d", opcode)
+	}
+	searchSpace := Range{Start: big.NewInt(117432), End: big.NewInt(117560), Index: index, Match: match, OutputLength: len(opCodes)}
+	ranges := findRanges(comp, searchSpace)
+
+	expectedRanges := []Range{
+		{Start: big.NewInt(117440), End: big.NewInt(117440), Index: searchSpace.Index - 1, Match: searchSpace.Match, OutputLength: len(opCodes)},
+	}
+
+	if len(ranges) != len(expectedRanges) {
+		t.Errorf("Expected %d ranges, but got %d", len(expectedRanges), len(ranges))
+	}
+
+	for i, r := range ranges {
+		compareRanges(t, r, expectedRanges[i], i)
+	}
+}
+
+func TestFindRangesWithBFSTest(t *testing.T) {
+	comp := day17.NewComputer()
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+	comp.SetOpcodes(opCodes)
+	index := len(opCodes)
+	initialRange := Range{Start: big.NewInt(0), End: big.NewInt(262144 - 8), Index: index - 0, Match: "0", OutputLength: len(opCodes)}
+	// Define expected results at each depth level
+	expectedResults := [][]Range{
+		{{Start: big.NewInt(0), End: big.NewInt(262136), Index: index - 1, Match: "0", OutputLength: len(opCodes)}},
+		{{Start: big.NewInt(98296), End: big.NewInt(163832), Index: index - 2, Match: "30", OutputLength: len(opCodes)}},
+		{{Start: big.NewInt(114680), End: big.NewInt(122872), Index: index - 3, Match: "430", OutputLength: len(opCodes)}},
+		{{Start: big.NewInt(117240), End: big.NewInt(118264), Index: index - 4, Match: "5430", OutputLength: len(opCodes)}},
+		{{Start: big.NewInt(117432), End: big.NewInt(117560), Index: index - 5, Match: "35430", OutputLength: len(opCodes)}},
+		{{Start: big.NewInt(117440), End: big.NewInt(117440), Index: index - 6, Match: "035430", OutputLength: len(opCodes)}},
+	}
+
+	// Initialize the heap
+	heap := []Range{initialRange}
+
+	for _, expected := range expectedResults {
+		// Pop the range from the heap
+		r := heap[0]
+		heap = heap[1:]
+
+		r.Match = ""
+		for _, opcode := range opCodes[r.Index-1:] {
+			r.Match += fmt.Sprintf("%d", opcode)
+		}
+
+		t.Logf("Index: %d: Processing range %s", r.Index, r)
+
+		// Find the ranges for the current range
+		ranges := findRanges(comp, r)
+		if len(ranges) != len(expected) {
+			t.Errorf("Expected %d ranges, but got %d", len(expected), len(ranges))
+			break
+		}
+		t.Logf("Index: %d: Found the correct number of ranges: %d", r.Index, len(ranges))
+
+		success := true
+		for j, e := range expected {
+			success = compareRanges(t, ranges[j], e, j)
+			if !success {
+				success = false
+			}
+		}
+		if !success {
+			break
+		}
+		t.Logf("Index: %d: Found the correct ranges", r.Index)
+
+		// Add the ranges to the heap
+		heap = append(heap, ranges...)
+	}
+}
+
+func TestFindRangesBFS(t *testing.T) {
+	opCodes := []day17.Opcode{0, 3, 5, 4, 3, 0}
+
+	results := findRangesBFS(opCodes)
+	expectedResults := []*big.Int{big.NewInt(117440)}
+
+	if len(results) != len(expectedResults) {
+		t.Errorf("Expected %d results, but got %d", len(expectedResults), len(results))
+	}
+
+	for i, r := range results {
+		if r.Cmp(expectedResults[i]) != 0 {
+			t.Errorf("Expected result %d to be %v, but got %v", i, expectedResults[i], r)
+		}
+	}
 }
